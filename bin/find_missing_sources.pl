e7ded9c3a8932fef9a28ffbfe439274dafefffdf
boconnor <boconnor>
2006-09-04 10:09:21 +0000

    A simple script to find missing source files

---
+++ bin/find_missing_sources.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+# a little script to find missing source files
+
+use strict;
+
+my @files = glob("SPECS/*.spec.in");
+
+foreach my $file (@files) {
+
+  open SPEC, "<$file" or die;
+  my $name;
+  my $version;
+  my @sources;
+
+  while(<SPEC>) {
+    chomp;
+    if (/^Source\d*:\s+(\S+)$/) { push @sources, $1; }
+    elsif (/^Version\d*:\s+(\S+)$/) { $version = $1; }
+    elsif (/^Name\d*:\s+(\S+)$/) { $name = $1; }
+    #elsif (/^Name\d*:\s+(\S+)$/) { $name = $1; }
+    #elsif (/^Name\d*:\s+(\S+)$/) { $name = $1; }
+    #elsif (/^Name\d*:\s+(\S+)$/) { $name = $1; }
+  }
+
+  foreach my $source (@sources) {
+    $source =~ s/\%\{name\}/$name/g;
+    $source =~ s/\%\{version\}/$version/g;
+    if (!-e "SOURCES/$source") { print "Probably missing: SOURCES/$source\n"; }
+  }
+  
+  close SPEC;
+
+}
